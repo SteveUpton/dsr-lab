d917bc836856efbe808b750afd59e1163e33beab
SteveUpton <steve.j.upton@googlemail.com>
2016-10-06 15:07:01 -0700

    Probably cleanup

--- frontend/app.py
+++ frontend/app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    r = requests.get("http://rongen:5001/")
-    return r.content
+    r = requests.get("http://rongen:5000/")
+    return "<b>" + r.content + "</b>"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
