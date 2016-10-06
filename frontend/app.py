a085acbbca4a22c2cc59c7829e4cc218f2ae35d9
SteveUpton <steve.j.upton@googlemail.com>
2016-10-06 15:54:39 -0700

    Many Bothans died to get this logger working

--- frontend/app.py
+++ frontend/app.py
@@ -9,4 +9,4 @@ def hello_world():
     return "<b>" + r.content + "</b>"
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=5001)
--- rongen/app.py
+++ rongen/app.py
@@ -1,7 +1,7 @@
 import time
 import zmq
 
-HOST = '127.0.0.1'
+HOST = '0.0.0.0'
 PORT = '4444'
 
 _context = zmq.Context()
@@ -35,4 +35,4 @@ def hello_world():
     return quote
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5001)
+    app.run(debug=True,host='0.0.0.0',port=5000)
